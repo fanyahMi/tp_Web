ff1b3e8f6c0ee7d54972fe7f3d11b777cabd13d2
fanyahMi <119622786+fanyahMi@users.noreply.github.com>
2023-11-21 16:21:05 +0100

    data

--- src/data.js
+++ src/data.js
@@ -2,25 +2,69 @@
 export const Joueur = [
     {
         id: 1,
-        nom: 'John Doe',
+        nom: 'Joel Embiid',
         equipe: 30,
-        mj: 'john@example.com',
-        ppm: 'john@example.com',
-        rpm: 'john@example.com',
-        pdpm: 'john@example.com',
-        mpm: 'john@example.com',
-        eff: 'john@example.com'
+        mj: 13,
+        ppm: 31.9,
+        rpm: 11.2,
+        pdpm: 6.2,
+        mpm: 34.1,
+        eff: 36.4
       },
       {
         id: 2,
-        nom: 'John Doe',
+        nom: 'Kevin Durant',
         equipe: 30,
-        mj: 'john@example.com',
-        ppm: 'test',
-        rpm: 'john@example.com',
-        pdpm: 'john@example.com',
-        mpm: 'john@example.com',
-        eff: 'john@example.com'
+        mj: 13,
+        ppm: 31.4,
+        rpm: 7.2,
+        pdpm: 5.5,
+        mpm: 37.2,
+        eff: 31.4
+      },
+      {
+        id: 3,
+        nom: 'Luka Doncic',
+        equipe: 30,
+        mj: 14,
+        ppm: 30.6,
+        rpm: 8,
+        pdpm: 8.1,
+        mpm: 34.6,
+        eff: 31.6
+      },
+      {
+        id: 4,
+        nom: 'Stephen Curry',
+        equipe: 30,
+        mj: 13,
+        ppm: 30.4,
+        rpm: 4.8,
+        pdpm: 3.6,
+        mpm: 33.2,
+        eff: 25.6
+      },
+      {
+        id: 5,
+        nom: 'Giannis Antetokoun',
+        equipe: 30,
+        mj: 13,
+        ppm: 30.2,
+        rpm: 10.2,
+        pdpm: 4.6,
+        mpm: 32.7,
+        eff: 32
+      },
+      {
+        id: 6,
+        nom: 'De Aaron Fox',
+        equipe: 30,
+        mj: 8,
+        ppm: 29.6,
+        rpm: 4.3,
+        pdpm: 5.4,
+        mpm: 35.1,
+        eff: 26.4
       },
 ];
 export default Joueur;
